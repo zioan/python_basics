d5698f3445ed28f3b714446697adc1fafeeb1d27
zioan <zaharia.ioan@gmail.com>
2021-08-04 21:17:46 +0200

    while_loops

---
+++ the_basics/while_loops.py
@@ -0,0 +1,30 @@
+#a = 3
+
+#while a > 0:
+#  print("working") #infinite loop :)
+
+##########
+
+#username = ''
+
+#while username != 'exit':
+  #username = input("Enter username: ")
+  #print(f"Hello {username}")
+
+a = 0
+while a < 5:
+    a = a + 1
+    print(a)
+
+##################
+
+username = ''
+
+while username != 'exit':
+  username = input("Enter username: ")
+  if username != 'exit':
+    print(f"Hello {username}")
+    print ("Type 'exit' to exit the program")
+  else :
+    print ("Program closed")
+  
---
+++ the_basics/while_loops_break_and_continue.py
@@ -0,0 +1,6 @@
+while True:
+  username = input("Enter username: ")
+  if username == 'exit':
+    break
+  else:
+    continue
